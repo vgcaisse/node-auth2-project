4aade771d92d4234f3e606db260571eba865d4d2
Visnu <vgcaisse@gmail.com>
2022-04-27 11:02:26 -0400

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -66,9 +66,9 @@ const validateRoleName = (req, res, next) => {
     }
   */
   if (!req.body.role_name || !req.body.role_name.trim()) {
-    req.role_name == 'student'
+    req.role_name = 'student'
     next()
-  } else if (req.body.role_name.trim() == 'admin') {
+  } else if (req.body.role_name.trim() === 'admin') {
     next({ status: 422, message: 'Role name can not be admin' })
   } else if (req.body.role_name.trim().length > 32) {
     next({ status: 422, message: 'Role name can not be longer than 32 chars' })
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -20,11 +20,12 @@ router.post("/register", validateRoleName, (req, res, next) => {
   const { username, password } = req.body
   const { role_name } = req
 
-  const hash = bcrypt.hashSync(password, 12)
+  const hash = bcrypt.hashSync(password, 8)
 
   User.add({ username, password: hash, role_name })
     .then(newUser => {
       res.status(201).json(newUser)
+      console.log('hitting')
     })
     .catch(err => {
       next(err)
